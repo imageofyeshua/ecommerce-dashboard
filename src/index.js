1f8f59ffd58dff43254f708b709c4d51b646ff3a
Daniel Park <intheimageofyeshua@gmail.com>
2024-09-04 22:11:24 +0900

    Created Reducer Store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React, { lazy, Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-const App = lazy(() => import('./App'));
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/index";
+const App = lazy(() => import("./App"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Suspense>
-      <App />
-    </Suspense>
+    <Provider store={store}>
+      <Suspense>
+        <App />
+      </Suspense>
+    </Provider>
   </BrowserRouter>
 );
 
